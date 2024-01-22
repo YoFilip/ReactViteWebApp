ab0a7660e3f0b4c5111c4a70835cbabaf234054a
Yo FIlip <filipxpp@wp.pl>
2024-01-22 23:24:32 +0100

    Fixing

--- src/components/Logos.jsx
+++ src/components/Logos.jsx
@@ -1,9 +1,5 @@
 import React from "react";
 
-import photo_discord_dark from '../assets/discord-dark.png';
-import photo_github_dark from '../assets/github-dark.png';
-import photo_email_dark from '../assets/email-dark.png';
-
 import photo_discord from '../assets/discord.png';
 import photo_github from '../assets/github.png';
 import photo_email from '../assets/email.png';
@@ -12,13 +8,13 @@ function Logos({ darkMode }) {
   return (
     <div>
       <a href="mailto:filipxpp@wp.pl">
-        <img src={darkMode ? photo_email_dark : photo_email} alt='Email icon' />
+        <img src={photo_email} alt='Email icon' />
       </a>
       <a href="https://github.com/YoFilip" target="_blank" rel="noopener noreferrer">
-        <img src={darkMode ? photo_github_dark : photo_github} alt='Github icon' />
+        <img src={photo_github} alt='Github icon' />
       </a>
       <a href="https://discord.gg/7zRPX4fk" target="_blank" rel="noopener noreferrer">
-        <img src={darkMode ? photo_discord_dark : photo_discord} alt='Discord icon' />
+        <img src={photo_discord} alt='Discord icon' />
       </a>
     </div>
   );
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -55,7 +55,6 @@ function Main() {
 		},
 
 
-
 	];
 
 	return (
