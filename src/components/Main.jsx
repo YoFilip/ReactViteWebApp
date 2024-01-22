15eef50d7fcc3ade97c59a170cd2917189315828
Yo FIlip <filipxpp@wp.pl>
2024-01-22 21:48:16 +0100

    Fixing

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,16 +5,17 @@ import ProjectBox from "./ProjectBox";
 import themeIconLight from "../assets/white_mode_logo.png";
 import themeIconDark from "../assets/dark_mode_logo.png";
 
-
-
-
 function Main() {
 	const [darkMode, setDarkMode] = React.useState(false);
 
-	function toggleTheme() {
+	  function toggleTheme() {
 		setDarkMode(!darkMode);
-	}
-
+		const body = document.body;
+		if (body) {
+		  body.classList.toggle("body-white-mode", darkMode);
+		  body.classList.toggle("body-dark-mode", !darkMode);
+		}
+	  }
 	const projects = [
 		{
 			title: "LazyCode",
@@ -56,38 +57,38 @@ function Main() {
 
 
 	];
+
 	return (
 		<div className={`container ${darkMode ? 'dark-mode' : ''}`}>
-			<div className='box box1'>
-				<Navbar_buttons />
-				<button className='theme-toggle-button' id="theme-toggle-button-mobile" onClick={toggleTheme}>
-					<h2 className="text">Dark Mode	</h2><img src={darkMode ? themeIconLight : themeIconDark} alt='Toggle theme' />
-				</button>
-				<Profile_box darkMode={darkMode} />
-				<Navbar_buttons />
+		  <div className='box box1'>
+			<Navbar_buttons />
+			<button className='theme-toggle-button' id="theme-toggle-button-mobile" onClick={toggleTheme}>
+			  <h2 className="text">Dark Mode</h2><img src={darkMode ? themeIconLight : themeIconDark} alt='Toggle theme' />
+			</button>
+			<Profile_box darkMode={darkMode} />
+			<Navbar_buttons />
+		  </div>
+		  <div className='box box2' id="box2">
+			<div className='projects-header text'>
+			  <h2>MY PROJECTS</h2>
+			  <button className='theme-toggle-button' onClick={toggleTheme}>
+				<img src={themeIconLight} alt='Toggle theme' />
+			  </button>
 			</div>
-			<div className='box box2' id="box2">
-				<div className='projects-header text'>
-					<h2>MY PROJECTS</h2>
-					<button className='theme-toggle-button' onClick={toggleTheme}>
-						<img src={themeIconLight} alt='Toggle theme' />
-					</button>
-				</div>
-				<div className='project-container'>
-					{projects.map((project) => (
-						<ProjectBox
-							key={project.title}
-							title={project.title}
-							description={project.description}
-							githubLink={project.githubLink}
-							liveDemoLink={project.liveDemoLink}
-						/>
-					))}
-				</div>
+			<div className='project-container'>
+			  {projects.map((project) => (
+				<ProjectBox
+				  key={project.title}
+				  title={project.title}
+				  description={project.description}
+				  githubLink={project.githubLink}
+				  liveDemoLink={project.liveDemoLink}
+				/>
+			  ))}
 			</div>
+		  </div>
 		</div>
-	);
-}
-
-
-export default Main;
+	  );
+	}
+	
+	export default Main;
